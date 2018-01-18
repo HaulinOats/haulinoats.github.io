e18a402a406e8fc698aab1cc8b7a7f83ee389b0a
Connolly <Brett.Connolly@mvwc.com>
2018-01-18 14:45:25 -0500

    updated api to v2

--- js/gmail.js
+++ js/gmail.js
@@ -1,14 +1,87 @@
 //GMail API info (only works for requests from this domain)
-var clientId = '239131988494-bdeh799j6rm275f41eedllvjk0lfqapc.apps.googleusercontent.com';
-var apiKey = 'Q8Q-q0zGGfCucsyxt5mWgWHN';
-var scopes ='https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/gmail.send';
+var CLIENT_ID = '239131988494-bdeh799j6rm275f41eedllvjk0lfqapc.apps.googleusercontent.com';
+// var apiKey = 'Q8Q-q0zGGfCucsyxt5mWgWHN';
+var SCOPES ='https://www.googleapis.com/auth/gmail.send';
+var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest"];
+
 function handleClientLoad() {
-    console.log('handleClientLoad()');
-    gapi.auth2.init({
-        client_id:clientId
-    })
-    .then(function(onInit, onError){
-        console.log(onInit);
-        console.log(onError);
-    })
+    gapi.load('client:auth2', initClient);
+}
+
+function initClient() {
+    gapi.client.init({
+      discoveryDocs: DISCOVERY_DOCS,
+      clientId: CLIENT_ID,
+      scope: SCOPES
+    }).then(function () {
+      // Listen for sign-in state changes.
+      gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+      // Handle the initial sign-in state.
+      updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+      authorizeButton.onclick = handleAuthClick;
+      signoutButton.onclick = handleSignoutClick;
+    });
+  }
+/**
+ *  Called when the signed in status changes, to update the UI
+ *  appropriately. After a sign-in, the API is called.
+ */
+function updateSigninStatus(isSignedIn) {
+    if (isSignedIn) {
+        authorizeButton.style.display = 'none';
+        signoutButton.style.display = 'block';
+        listLabels();
+    } else {
+        authorizeButton.style.display = 'block';
+        signoutButton.style.display = 'none';
+    }
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick(event) {
+    gapi.auth2.getAuthInstance().signIn();
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick(event) {
+    gapi.auth2.getAuthInstance().signOut();
+}
+
+/**
+ * Append a pre element to the body containing the given message
+ * as its text node. Used to display the results of the API call.
+ *
+ * @param {string} message Text to be placed in pre element.
+ */
+function appendPre(message) {
+    var pre = document.getElementById('content');
+    var textContent = document.createTextNode(message + '\n');
+    pre.appendChild(textContent);
+}
+
+/**
+ * Print all Labels in the authorized user's inbox. If no labels
+ * are found an appropriate message is printed.
+ */
+function listLabels() {
+    gapi.client.gmail.users.labels.list({
+        'userId': 'me'
+    }).then(function(response) {
+        var labels = response.result.labels;
+        appendPre('Labels:');
+
+        if (labels && labels.length > 0) {
+        for (i = 0; i < labels.length; i++) {
+            var label = labels[i];
+            appendPre(label.name)
+        }
+        } else {
+        appendPre('No Labels found.');
+        }
+    });
 }
