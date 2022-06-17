461550bf3df48f88fec13423c7e20a962c8d30c3
Brett Connolly <brett84c@gmail.com>
2022-06-17 19:05:49 -0400

    update tech section to be more dynamic

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,129 @@
-$( document ).ready(function() {
+$(document).ready(function () {
+  var experience_level_array = [];
 
-	//Randomize numbers in 'languages and techonlogies' section
-	var interval_count = 0;
-	var experience_level_array = [];
+  $(".right li").each(function () {
+    experience_level_array.push($(this).text());
+  });
 
-	$('.right li').each(function(){
-		experience_level_array.push($(this).text());
-	}) 	
+  var li_height = $(".left ul li").first().height();
+  $(".right ul li").height(li_height);
 
-	var li_height = $('.left ul li').first().height();
-	$('.right ul li').height(li_height);
-	
-     calculateRatio();
+  calculateRatio();
 
-     function calculateRatio() {
-          var ratio = $(window).height()/$(window).width();
-          $('.tech-section ul li').height(40 * ratio);
-     }
+  function calculateRatio() {
+    var ratio = $(window).height() / $(window).width();
+    $(".tech-section ul li").height(40 * ratio);
+  }
 
-     $(window).resize(function() {
-          calculateRatio();
-     });
+  $(window).resize(function () {
+    calculateRatio();
+  });
+
+  //Contact form
+  emailjs.init("user_edxN8J1IcxZ58snUCCvmB");
+
+  // Contact Form Submit
+  document
+    .querySelector("#contact-form")
+    .addEventListener("submit", function (e) {
+      e.preventDefault();
+      emailjs
+        .sendForm("service_brnc745", "template_u86d2ao", "#contact-form")
+        .then((resp) => {
+          document.querySelector("#contact-sent").style.opacity = "1";
+        })
+        .catch((err) => {
+          document.querySelector("#contact-error").style.opacity = "1";
+        });
+    });
+
+  //populate skills section
+  const skills = {
+    languages: [
+      { text: "HTML5", level: 5 },
+      { text: "CSS3", level: 5 },
+      { text: "Javascript", level: 5 },
+      { text: "Typescript", level: 4 },
+      { text: "NodeJS", level: 4 },
+      { text: "PHP", level: 3 },
+      { text: "SQL", level: 3 },
+      { text: "Ruby/Rails", level: 3 },
+      { text: "Java", level: 2 },
+      { text: "Python", level: 2 },
+    ],
+    frameworks: [
+      { text: "React", level: 5 },
+      { text: "Svelte", level: 4 },
+      { text: "MeteorJS", level: 4 },
+      { text: "Express", level: 4 },
+      { text: "Electron", level: 4 },
+      { text: "Laravel", level: 3 },
+      { text: "Vue", level: 3 },
+      { text: "Webpack", level: 3 },
+      { text: "Angular (2+)", level: 3 },
+      { text: "Wordpress", level: 3 },
+    ],
+    other: [
+      { text: "Visual Studio Code", level: 5 },
+      { text: "MongoDB", level: 4 },
+      { text: "MySQL", level: 4 },
+      { text: "Phonegap/Cordova", level: 4 },
+      { text: "REST APIs", level: 4 },
+      { text: "Git/SVN", level: 4 },
+      { text: "JSON/XML", level: 4 },
+      { text: "Active Record", level: 3 },
+      { text: "Object-Oriented", level: 3 },
+      { text: "Command Line", level: 3 },
+    ],
+  };
+
+  const skillText = {
+    1: "Noob",
+    2: "Beginner",
+    3: "Intermediate",
+    4: "Advanced",
+    5: "Fluent",
+  };
+
+  let languagesHTML = "";
+  let frameworksHTML = "";
+  let otherHTML = "";
+
+  for (const key in skills) {
+    switch (key) {
+      case "languages":
+        languagesHTML = getTechSectionHTML(skills[key]);
+        break;
+      case "frameworks":
+        frameworksHTML = getTechSectionHTML(skills[key]);
+        break;
+      case "other":
+        otherHTML = getTechSectionHTML(skills[key]);
+        break;
+    }
+  }
+
+  document.getElementById("languages-section").innerHTML = languagesHTML;
+  document.getElementById("frameworks-section").innerHTML = frameworksHTML;
+  document.getElementById("other-section").innerHTML = otherHTML;
+
+  function getTechSectionHTML(itemArr) {
+    return itemArr
+      .map((item) => {
+        return `
+				<div class="tech-section-inner-row">
+					<div class="tech-section-inner-row-left">
+						<p>${item.text}</p>
+					</div>
+					<div class="tech-section-inner-row-right">
+						<div data-skill="${item.level}"></div>
+						<div>
+							<p class="skill-text">${skillText[item.level]}</p>
+						</div>
+					</div>
+				</div>
+			`;
+      })
+      .join("");
+  }
 });
--- js/designr.js
+++ js/designr.js
@@ -1,304 +1,293 @@
-$(document).ready(function() {
-
-	/*============================================
+$(document).ready(function () {
+  /*============================================
 	Navigation Functions
 	==============================================*/
-	if ($(window).scrollTop()===0){
-		$('#main-nav').removeClass('scrolled');
-	}
-	else{
-		$('#main-nav').addClass('scrolled');    
-	}
+  if ($(window).scrollTop() === 0) {
+    $("#main-nav").removeClass("scrolled");
+  } else {
+    $("#main-nav").addClass("scrolled");
+  }
 
-	$(window).scroll(function(){
-		if ($(window).scrollTop()===0){
-			$('#main-nav').removeClass('scrolled');
-		}
-		else{
-			$('#main-nav').addClass('scrolled');    
-		}
-	});
+  $(window).scroll(function () {
+    if ($(window).scrollTop() === 0) {
+      $("#main-nav").removeClass("scrolled");
+    } else {
+      $("#main-nav").addClass("scrolled");
+    }
+  });
 
-	/*============================================
+  /*============================================
 	ScrollTo Links
 	==============================================*/
-	$('a.scrollto').click(function(e){
-		$('html,body').scrollTo(this.hash, this.hash, {gap:{y:-80}});
-		e.preventDefault();
+  $("a.scrollto").click(function (e) {
+    $("html,body").scrollTo(this.hash, this.hash, { gap: { y: -80 } });
+    e.preventDefault();
 
-		if ($('.navbar-collapse').hasClass('in')){
-			$('.navbar-collapse').removeClass('in').addClass('collapse');
-		}
-	});
+    if ($(".navbar-collapse").hasClass("in")) {
+      $(".navbar-collapse").removeClass("in").addClass("collapse");
+    }
+  });
 
-	/*============================================
+  /*============================================
 	Header Functions
 	==============================================*/
-	$('.jumbotron').height($(window).height()+50);
-	$('.message-box').css({'marginTop':$(window).height()*0.4});
+  $(".jumbotron").height($(window).height() + 50);
+  $(".message-box").css({ marginTop: $(window).height() * 0.4 });
 
-	$('.home-slider').flexslider({
-		animation: "slide",
-		directionNav: false,
-		controlNav: false,
-		direction: "vertical",
-		slideshowSpeed: 2500,
-		animationSpeed: 500,
-		smoothHeight: true
-	});
+  $(".home-slider").flexslider({
+    animation: "slide",
+    directionNav: false,
+    controlNav: false,
+    direction: "vertical",
+    slideshowSpeed: 2500,
+    animationSpeed: 500,
+    smoothHeight: true,
+  });
 
-	/*============================================
+  /*============================================
 	Skills Functions
 	==============================================*/
-	var aboutColor = $('#about').css('backgroundColor');
+  var aboutColor = $("#about").css("backgroundColor");
 
-	$('#skills').waypoint(function(){
-		animateTechnologyBars();
-		$('.chart').each(function(){
-		$(this).easyPieChart({
-				size:170,
-				animate: 2000,
-				lineCap:'butt',
-				scaleColor: false,
-				barColor: aboutColor,
-				lineWidth: 10
-			});
-		});
-	},{offset:'80%'});
+  $("#skills").waypoint(
+    function () {
+      animateTechnologyBars();
+      $(".chart").each(function () {
+        $(this).easyPieChart({
+          size: 170,
+          animate: 2000,
+          lineCap: "butt",
+          scaleColor: false,
+          barColor: aboutColor,
+          lineWidth: 10,
+        });
+      });
+    },
+    { offset: "80%" }
+  );
 
-	// $('#technologies').waypoint(function(){
-	// 	animateTechnologyBars();
-	// });
-	// $('.tech-cont').mouseover(function(){
-	// 	animateTechnologyBars();
-	// });
-	// $('#technologies').waypoint(function(){
-	// 	animateTechnologyBars();
-	// });
+  // $('#technologies').waypoint(function(){
+  // 	animateTechnologyBars();
+  // });
+  // $('.tech-cont').mouseover(function(){
+  // 	animateTechnologyBars();
+  // });
+  // $('#technologies').waypoint(function(){
+  // 	animateTechnologyBars();
+  // });
 
-	function animateTechnologyBars(){
-		$('.skill-level').each(function(){
-			var skill_level = $(this).attr('data-skill');
-			var element = this;
-			switch (skill_level) {
-			  case "1":
-			    $(element).css({'width':'20%', 'background-color':'#F2473F'});
-			    break;
-			  case "2":
-			    $(element).css({'width':'40%', 'background-color':'#FA9A50'});
-			    break;
-			  case "3":
-			    $(element).css({'width':'60%', 'background-color':'#FFEC8B'});
-			    break;
-			  case "4":
-			    $(element).css('width','80%');
-			    break;
-			  case "5":
-			    $(element).css({'width':'100%', 'background-color':'#9CCB19'});
-			    break;
-			}	
-		});
-	}
-	
-	/*============================================
+  function animateTechnologyBars() {
+    $(".skill-level").each(function () {
+      var skill_level = $(this).attr("data-skill");
+      var element = this;
+      switch (skill_level) {
+        case "1":
+          $(element).css({ width: "20%", "background-color": "#F2473F" });
+          break;
+        case "2":
+          $(element).css({ width: "40%", "background-color": "#FA9A50" });
+          break;
+        case "3":
+          $(element).css({ width: "60%", "background-color": "#FFEC8B" });
+          break;
+        case "4":
+          $(element).css("width", "80%");
+          break;
+        case "5":
+          $(element).css({ width: "100%", "background-color": "#9CCB19" });
+          break;
+      }
+    });
+  }
+
+  /*============================================
 	Project thumbs - Masonry
 	==============================================*/
-	$(window).load(function(){
+  $(window).load(function () {
+    $("#projects-container").css({ visibility: "visible" });
 
-		$('#projects-container').css({visibility:'visible'});
+    $("#projects-container").masonry({
+      itemSelector: ".project-item:not(.filtered)",
+      columnWidth: 350,
+      isFitWidth: true,
+      isResizable: true,
+      isAnimated: !Modernizr.csstransitions,
+      gutterWidth: 0,
+    });
 
-		$('#projects-container').masonry({
-			itemSelector: '.project-item:not(.filtered)',
-			columnWidth:350,
-			isFitWidth: true,
-			isResizable: true,
-			isAnimated: !Modernizr.csstransitions,
-			gutterWidth: 0
-		});
+    scrollSpyRefresh();
+    waypointsRefresh();
+  });
 
-		scrollSpyRefresh();
-		waypointsRefresh();
-	});
-
-	/*============================================
+  /*============================================
 	Filter Projects
 	==============================================*/
-	$('#filter-works a').click(function(e){
-		e.preventDefault();
+  $("#filter-works a").click(function (e) {
+    e.preventDefault();
 
-		$('#filter-works li').removeClass('active');
-		$(this).parent('li').addClass('active');
+    $("#filter-works li").removeClass("active");
+    $(this).parent("li").addClass("active");
 
-		var category = $(this).attr('data-filter');
+    var category = $(this).attr("data-filter");
 
-		$('.project-item').each(function(){
-			if($(this).is(category)){
-				$(this).removeClass('filtered');
-			}
-			else{
-				$(this).addClass('filtered');
-			}
+    $(".project-item").each(function () {
+      if ($(this).is(category)) {
+        $(this).removeClass("filtered");
+      } else {
+        $(this).addClass("filtered");
+      }
 
-			$('#projects-container').masonry('reload');
-		});
+      $("#projects-container").masonry("reload");
+    });
 
-		scrollSpyRefresh();
-		waypointsRefresh();
-	});
+    scrollSpyRefresh();
+    waypointsRefresh();
+  });
 
-	/*============================================
+  /*============================================
 	Project Preview
 	==============================================*/
-	$('.project-item').click(function(e){
-		e.preventDefault();
+  $(".project-item").click(function (e) {
+    e.preventDefault();
 
-		var elem = $(this),
-			title = elem.find('.project-title').text(),
-			link = elem.attr('href'),
-			descr = elem.find('.project-description').html(),
-			slidesHtml = '<ul class="slides">',
+    var elem = $(this),
+      title = elem.find(".project-title").text(),
+      link = elem.attr("href"),
+      descr = elem.find(".project-description").html(),
+      slidesHtml = '<ul class="slides">',
+      slides = elem.data("images").split(",");
 
-			slides = elem.data('images').split(',');
+    for (var i = 0; i < slides.length; ++i) {
+      slidesHtml = slidesHtml + "<li><img src=" + slides[i] + ' alt=""></li>';
+    }
 
-		for (var i = 0; i < slides.length; ++i) {
-			slidesHtml = slidesHtml + '<li><img src='+slides[i]+' alt=""></li>';
-		}
-		
-		slidesHtml = slidesHtml + '</ul>';
-		
+    slidesHtml = slidesHtml + "</ul>";
 
-		$('#project-modal').on('show.bs.modal', function () {
-			$(this).find('h1').text(title);
-			$(this).find('.btn').attr('href', link).attr('target', '_blank');
-			$(this).find('.project-descr').html(descr);
-			$(this).find('.image-wrapper').addClass('flexslider').html(slidesHtml);
-			
-			setTimeout(function(){
-				$('.image-wrapper.flexslider').flexslider({
-					slideshowSpeed: 3000,
-					animation: 'slide',
-					controlNav: false,
-					start: function(){
-						$('#project-modal .image-wrapper')
-						.addClass('done')
-						.prev('.loader').fadeOut();
-					}
-				});
-			},1000);
-		}).modal();
-	});
+    $("#project-modal")
+      .on("show.bs.modal", function () {
+        $(this).find("h1").text(title);
+        $(this).find(".btn").attr("href", link).attr("target", "_blank");
+        $(this).find(".project-descr").html(descr);
+        $(this).find(".image-wrapper").addClass("flexslider").html(slidesHtml);
 
-	$('#project-modal').on('hidden.bs.modal', function () {
-		$(this).find('.loader').show();
-		$(this).find('.image-wrapper')
-			.removeClass('flexslider')
-			.removeClass('done')
-			.html('')
-			.flexslider('destroy');
-	});
-	
-	/*============================================
+        setTimeout(function () {
+          $(".image-wrapper.flexslider").flexslider({
+            slideshowSpeed: 3000,
+            animation: "slide",
+            controlNav: false,
+            start: function () {
+              $("#project-modal .image-wrapper")
+                .addClass("done")
+                .prev(".loader")
+                .fadeOut();
+            },
+          });
+        }, 1000);
+      })
+      .modal();
+  });
+
+  $("#project-modal").on("hidden.bs.modal", function () {
+    $(this).find(".loader").show();
+    $(this)
+      .find(".image-wrapper")
+      .removeClass("flexslider")
+      .removeClass("done")
+      .html("")
+      .flexslider("destroy");
+  });
+
+  /*============================================
 	Twitter Functions
 	==============================================*/
-	// var tweetsLength = $('#twitter-slider').data('tweets-length'),
-	// 	widgetID = $('#twitter-slider').data('widget-id');
-	
-	// twitterFetcher.fetch(widgetID, 'twitter-slider', tweetsLength, true, false, true, '', false, handleTweets);
+  // var tweetsLength = $('#twitter-slider').data('tweets-length'),
+  // 	widgetID = $('#twitter-slider').data('widget-id');
 
-	// function handleTweets(tweets){
-	
-	// 	var x = tweets.length,
-	// 		n = 0,
-	// 		tweetsHtml = '<ul class="slides">';
-			
-	// 	while(n < x) {
-	// 		tweetsHtml += '<li>' + tweets[n] + '</li>';
-	// 		n++;
-	// 	}
-		
-	// 	tweetsHtml += '</ul>';
-	// 	$('#twitter-slider').html(tweetsHtml);
-		
-	// 	$('.twitter_reply_icon').html("<i class='icon-reply'></i>");
-	// 	$('.twitter_retweet_icon').html("<i class='icon-retweet'></i>");
-	// 	$('.twitter_fav_icon').html("<i class='icon-star'></i>");
+  // twitterFetcher.fetch(widgetID, 'twitter-slider', tweetsLength, true, false, true, '', false, handleTweets);
 
-	// 	$('#twitter-slider').flexslider({
-	// 		prevText: '<i class="icon-angle-left"></i>',
-	// 		nextText: '<i class="icon-angle-right"></i>',
-	// 		slideshowSpeed: 5000,
-	// 		useCSS: true,
-	// 		controlNav: false, 
-	// 		pauseOnAction: false, 
-	// 		pauseOnHover: true,
-	// 		smoothHeight: false
-	// 	});
-	// }
-	/*============================================
+  // function handleTweets(tweets){
+
+  // 	var x = tweets.length,
+  // 		n = 0,
+  // 		tweetsHtml = '<ul class="slides">';
+
+  // 	while(n < x) {
+  // 		tweetsHtml += '<li>' + tweets[n] + '</li>';
+  // 		n++;
+  // 	}
+
+  // 	tweetsHtml += '</ul>';
+  // 	$('#twitter-slider').html(tweetsHtml);
+
+  // 	$('.twitter_reply_icon').html("<i class='icon-reply'></i>");
+  // 	$('.twitter_retweet_icon').html("<i class='icon-retweet'></i>");
+  // 	$('.twitter_fav_icon').html("<i class='icon-star'></i>");
+
+  // 	$('#twitter-slider').flexslider({
+  // 		prevText: '<i class="icon-angle-left"></i>',
+  // 		nextText: '<i class="icon-angle-right"></i>',
+  // 		slideshowSpeed: 5000,
+  // 		useCSS: true,
+  // 		controlNav: false,
+  // 		pauseOnAction: false,
+  // 		pauseOnHover: true,
+  // 		smoothHeight: false
+  // 	});
+  // }
+  /*============================================
 	Resize Functions
 	==============================================*/
-	$(window).resize(function(){
-		$('.jumbotron').height($(window).height());
-		$('.message-box').css({'marginTop':$(window).height()*0.4});
-		scrollSpyRefresh();
-		waypointsRefresh();
-	});
+  $(window).resize(function () {
+    $(".jumbotron").height($(window).height());
+    $(".message-box").css({ marginTop: $(window).height() * 0.4 });
+    scrollSpyRefresh();
+    waypointsRefresh();
+  });
 
-	/*============================================
+  /*============================================
 	Project Hover mask on IE
 	==============================================*/
-	$('.no-csstransitions .hover-mask').hover(
-		function() {
-			$( this ).stop(true,true).animate({opacity: 1});
-		}, function() {
-			$( this ).stop(true,true).animate({opacity: 0});
-		}
-	);
-	
-	/*============================================
+  $(".no-csstransitions .hover-mask").hover(
+    function () {
+      $(this).stop(true, true).animate({ opacity: 1 });
+    },
+    function () {
+      $(this).stop(true, true).animate({ opacity: 0 });
+    }
+  );
+
+  /*============================================
 	Placeholder Detection
 	==============================================*/
-	if (!Modernizr.input.placeholder) {
-		$('#contact-form').addClass('no-placeholder');
-	}
+  if (!Modernizr.input.placeholder) {
+    $("#contact-form").addClass("no-placeholder");
+  }
 
-	/*============================================
+  /*============================================
 	Scrolling Animations
 	==============================================*/
-	$('.scrollimation').waypoint(function(){
-		$(this).toggleClass('in');
-	},{offset:'90%'});
+  $(".scrollimation").waypoint(
+    function () {
+      $(this).toggleClass("in");
+    },
+    { offset: "90%" }
+  );
 
-	/*============================================
+  /*============================================
 	Refresh scrollSpy function
 	==============================================*/
-	function scrollSpyRefresh(){
-		setTimeout(function(){
-			$('body').scrollspy('refresh');
-		},1000);
-	}
+  function scrollSpyRefresh() {
+    setTimeout(function () {
+      $("body").scrollspy("refresh");
+    }, 1000);
+  }
 
-	/*============================================
+  /*============================================
 	Refresh waypoints function
 	==============================================*/
-	function waypointsRefresh(){
-		setTimeout(function(){
-			$.waypoints('refresh');
-		},1000);
-	}
-
-	//Contact form
-	emailjs.init("user_edxN8J1IcxZ58snUCCvmB");
-
-	// Contact Form Submit
-	document.querySelector('#contact-form').addEventListener('submit', function(e){
-    e.preventDefault();
-    emailjs.sendForm('service_brnc745','template_u86d2ao', '#contact-form')
-    .then(resp => {
-      document.querySelector('#contact-sent').style.opacity = '1';
-    })
-    .catch(err =>{
-      document.querySelector('#contact-error').style.opacity = '1';
-    })
-	}); 
-});	
+  function waypointsRefresh() {
+    setTimeout(function () {
+      $.waypoints("refresh");
+    }, 1000);
+  }
+});
