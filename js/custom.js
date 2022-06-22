ef2cac1313910045c544951808c970d53208b218
Brett Connolly <brett84c@gmail.com>
2022-06-22 14:02:15 -0400

    updated technologies section and some copy

--- js/custom.js
+++ js/custom.js
@@ -40,40 +40,40 @@ $(document).ready(function () {
   //populate skills section
   const skills = {
     languages: [
-      { text: "HTML5", level: 5 },
-      { text: "CSS3", level: 5 },
-      { text: "Javascript", level: 5 },
-      { text: "Typescript", level: 4 },
-      { text: "NodeJS", level: 4 },
-      { text: "PHP", level: 3 },
-      { text: "SQL", level: 3 },
-      { text: "Ruby/Rails", level: 3 },
-      { text: "Java", level: 2 },
-      { text: "Python", level: 2 },
+      { text: "HTML5", level: 5, logo: "HTML5_logo.svg" },
+      { text: "CSS3", level: 5, logo: "CSS3_logo.svg" },
+      { text: "Javascript", level: 5, logo: "JS_logo.svg" },
+      { text: "Typescript", level: 4, logo: "TS_logo.svg" },
+      { text: "NodeJS", level: 4, logo: "Node_logo.svg" },
+      { text: "PHP", level: 3, logo: "PHP_logo.svg" },
+      { text: "SQL", level: 3, logo: "MySQL_logo.png" },
+      { text: "Ruby/Rails", level: 3, logo: "Ruby_logo.svg" },
+      { text: "Java", level: 2, logo: "Java_logo.svg" },
+      { text: "Python", level: 2, logo: "Python_logo.svg" },
     ],
     frameworks: [
-      { text: "React", level: 5 },
-      { text: "Svelte", level: 4 },
-      { text: "MeteorJS", level: 4 },
-      { text: "Express", level: 4 },
-      { text: "Electron", level: 4 },
-      { text: "Laravel", level: 3 },
-      { text: "Vue", level: 3 },
-      { text: "Webpack", level: 3 },
-      { text: "Angular (2+)", level: 3 },
-      { text: "Wordpress", level: 3 },
+      { text: "React", level: 5, logo: "React_logo.svg" },
+      { text: "Svelte", level: 4, logo: "Svelte_logo.svg" },
+      { text: "MeteorJS", level: 4, logo: "Meteor_logo.svg" },
+      { text: "Express", level: 4, logo: "Express_logo.png" },
+      { text: "Electron", level: 4, logo: "Electron_logo.svg" },
+      { text: "Laravel", level: 3, logo: "Laravel_logo.svg" },
+      { text: "Vue", level: 3, logo: "Vue_logo.svg" },
+      { text: "Webpack", level: 3, logo: "Webpack_logo.svg" },
+      { text: "Angular (2+)", level: 3, logo: "Angular_logo.svg" },
+      { text: "Wordpress", level: 3, logo: "Wordpress_logo.svg" },
     ],
     other: [
-      { text: "Visual Studio Code", level: 5 },
-      { text: "MongoDB", level: 4 },
-      { text: "MySQL", level: 4 },
-      { text: "Phonegap/Cordova", level: 4 },
-      { text: "REST APIs", level: 4 },
-      { text: "Git/SVN", level: 4 },
-      { text: "JSON/XML", level: 4 },
-      { text: "Active Record", level: 3 },
-      { text: "Object-Oriented", level: 3 },
-      { text: "Command Line", level: 3 },
+      { text: "Visual Studio Code", level: 5, logo: "VSCode_logo.svg" },
+      { text: "MongoDB", level: 4, logo: "MongoDB_logo.svg" },
+      { text: "MySQL", level: 4, logo: "MySQL_logo.png" },
+      { text: "Phonegap/Cordova", level: 4, logo: "phonegap_logo.svg" },
+      { text: "REST APIs", level: 4, logo: "REST_logo.svg" },
+      { text: "Git/SVN", level: 4, logo: "Git_logo.svg" },
+      { text: "JSON/XML", level: 4, logo: "JSON_logo.svg" },
+      { text: "Active Record", level: 3, logo: "Rails_logo.png" },
+      { text: "Object-Oriented", level: 3, logo: "OOP_logo.svg" },
+      { text: "Command Line", level: 3, logo: "Terminal_logo.svg" },
     ],
   };
 
@@ -113,6 +113,15 @@ $(document).ready(function () {
         return `
 				<div class="tech-section-inner-row">
 					<div class="tech-section-inner-row-left">
+            ${
+              item.logo
+                ? `<img
+                  class="tech-icon"
+                  alt="icon"
+                  src="./img/icons/${item.logo}"
+                />`
+                : ``
+            }
 						<p>${item.text}</p>
 					</div>
 					<div class="tech-section-inner-row-right">
